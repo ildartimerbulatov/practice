85987bf71d37d4ee4785e496821123705126d22e
Ildar <ildartimerbulatov@yandex.ru>
2024-05-26 21:31:37 +0300

    создали файлы

---
+++ code.cs
@@ -0,0 +1,45 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод исходного массива строк
+        Console.Write("Введите исходный массив строк через пробел: ");
+        string[] inputArray = Console.ReadLine().Split();
+
+        // Создание нового массива с подходящими строками
+        string[] outputArray = CreateOutputArray(inputArray);
+
+        // Вывод результата
+        Console.WriteLine("Новый массив с элементами длиной 3 символа или меньше:");
+        foreach (string s in outputArray)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    static string[] CreateOutputArray(string[] inputArray)
+    {
+        int count = 0;
+        foreach (string s in inputArray)
+        {
+            if (s.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] outputArray = new string[count];
+        int index = 0;
+        foreach (string s in inputArray)
+        {
+            if (s.Length <= 3)
+            {
+                outputArray[index++] = s;
+            }
+        }
+
+        return outputArray;
+    }
+}
