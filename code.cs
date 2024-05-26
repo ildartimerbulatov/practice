518a527fb868a72e10612c706f0977633c329ea5
Ildar <ildartimerbulatov@yandex.ru>
2024-05-26 22:33:42 +0300

    корректровка комменты code end

--- code.cs
+++ code.cs
@@ -1,4 +1,5 @@
 using System;
+
 class Program
 {
     static void Main(string[] args)
@@ -6,3 +7,39 @@ class Program
         // Ввод исходного массива строк
         Console.Write("Введите исходный массив строк через пробел: ");
         string[] inputArray = Console.ReadLine().Split();
+
+        // Создание нового массива с подходящими строками
+        string[] outputArray = CreateOutputArray(inputArray);
+
+        // Вывод результата
+        Console.WriteLine("Новый массив с элементами длиной 3 символа или меньше:");
+        foreach (string s in outputArray)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    static string[] CreateOutputArray(string[] inputArray)
+    {
+        int count = 0;
+        foreach (string s in inputArray)
+        {
+            if (s.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] outputArray = new string[count];
+        int index = 0;
+        foreach (string s in inputArray)
+        {
+            if (s.Length <= 3)
+            {
+                outputArray[index++] = s;
+            }
+        }
+
+        return outputArray;
+    }
+}
