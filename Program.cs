06b4bb77d894956bfee7e252de06b298d4ac1952
Ildar <ildartimerbulatov@yandex.ru>
2024-05-27 00:23:20 +0300

    корректровка комменты code end

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод исходного массива строк
+        Console.Write("Введите исходный массив строк через пробел: ");
+        string[] inputArray = Console.ReadLine().Split();
